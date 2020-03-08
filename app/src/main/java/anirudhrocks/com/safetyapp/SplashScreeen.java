cff4e4e4ece4c6b805b7ca0ab0804a133f71fb10
vishalmanes109 <36571896+vishalmanes109@users.noreply.github.com>
2020-03-08 12:11:39 +0530

    splash screen

--- app/src/main/java/anirudhrocks/com/safetyapp/SplashScreeen.java
+++ app/src/main/java/anirudhrocks/com/safetyapp/SplashScreeen.java
@@ -1,8 +1,10 @@
 package anirudhrocks.com.safetyapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreeen extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SplashScreeen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screeen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i= new Intent(SplashScreeen.this,ContactActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
     }
 }
